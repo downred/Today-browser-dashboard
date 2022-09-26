1ee65d8946563da5b0d2e503cda0a0be1c2b8ea4
downred <redonqerimi@gmail.com>
2022-09-26 07:49:20 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import "./App.css";
 import Time from "./components/time";
 import { FaMapMarkerAlt } from "react-icons/fa";
+import CurrentWeather from "./components/current-weather";
 
 function App() {
   return (
     <div className="App bg">
       <div className="time-wrapper">
-        <Time className="time" />
-        <h4 className="location">
-          <span>
-            <FaMapMarkerAlt /> Fushe Kosove, Kosovo
-          </span>
-        </h4>
+        <Time />
+        <div className="location">
+          <FaMapMarkerAlt/>
+          <span>Fushe Kosove, Kosovo</span>
+        </div>
+      </div>
+      <div className="pg-bottom">
+        <CurrentWeather/>
+        <CurrentWeather/>
       </div>
     </div>
   );
---
+++ src/components/current-weather.jsx
@@ -0,0 +1,9 @@
+import "./modules/current-weather.css"
+
+const CurrentWeather = () => {
+    return ( 
+        <div className="box"></div>
+     );
+}
+ 
+export default CurrentWeather;
