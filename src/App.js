981f6ec3fdad346b6002d747be668bf214776625
downred <redonqerimi@gmail.com>
2022-09-26 12:02:18 +0200

    add content to weather container

--- src/App.js
+++ src/App.js
@@ -9,13 +9,15 @@ function App() {
       <div className="time-wrapper">
         <Time />
         <div className="location">
-          <FaMapMarkerAlt/>
+          <FaMapMarkerAlt />
           <span>Fushe Kosove, Kosovo</span>
         </div>
       </div>
       <div className="pg-bottom">
-        <CurrentWeather/>
-        <CurrentWeather/>
+        <CurrentWeather />
+        <div className="test">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit incidunt culpa explicabo qui inventore maiores dolor error possimus unde reprehenderit. Quia similique facilis inventore sequi, optio id, eos delectus provident voluptas omnis ex qui fugit accusamus animi at? Sapiente, eligendi. Officia ipsa libero aperiam quod rem impedit eligendi unde incidunt quaerat sed illum molestias quisquam possimus quo aliquam commodi excepturi facere vero enim explicabo officiis omnis blanditiis, voluptatem expedita? Magni vitae inventore autem facilis culpa voluptas blanditiis provident eum? Quaerat aliquam omnis et commodi corrupti placeat hic repellendus praesentium unde aspernatur! Libero, autem id. Nobis quia voluptatibus possimus consequatur. Voluptatum.</p>
+        </div>
       </div>
     </div>
   );
--- src/components/current-weather.jsx
+++ src/components/current-weather.jsx
@@ -1,9 +1,14 @@
-import "./modules/current-weather.css"
+import "./modules/current-weather.css";
 
 const CurrentWeather = () => {
-    return ( 
-        <div className="box"></div>
-     );
-}
- 
+  return (
+    <div className="box">
+      <div className="weather">
+        <span className="description">Overcast</span>
+        <span className="temp">10°C</span>
+      </div>
+    </div>
+  );
+};
+
 export default CurrentWeather;
