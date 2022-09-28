3d112a6a111634ccfa223ad52e59a1d4ebde03c3
downred <redonqerimi@gmail.com>
2022-09-28 09:38:53 +0200

    add weather details to weather container

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
       <div className="pg-bottom">
         <CurrentWeather />
         <div className="test">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit incidunt culpa explicabo qui inventore maiores dolor error possimus unde reprehenderit. Quia similique facilis inventore sequi, optio id, eos delectus provident voluptas omnis ex qui fugit accusamus animi at? Sapiente, eligendi. Officia ipsa libero aperiam quod rem impedit eligendi unde incidunt quaerat sed illum molestias quisquam possimus quo aliquam commodi excepturi facere vero enim explicabo officiis omnis blanditiis, voluptatem expedita? Magni vitae inventore autem facilis culpa voluptas blanditiis provident eum? Quaerat aliquam omnis et commodi corrupti placeat hic repellendus praesentium unde aspernatur! Libero, autem id. Nobis quia voluptatibus possimus consequatur. Voluptatum.</p>
+          <p></p>
         </div>
       </div>
     </div>
--- src/components/current-weather.jsx
+++ src/components/current-weather.jsx
@@ -1,4 +1,5 @@
 import "./modules/current-weather.css";
+import { FaWind } from "react-icons/fa";
 
 const CurrentWeather = () => {
   return (
@@ -6,6 +7,31 @@ const CurrentWeather = () => {
       <div className="weather">
         <span className="description">Overcast</span>
         <span className="temp">10°C</span>
+        <div className="details">
+          <span>Feels like</span>
+          <span className="detail">11°C</span>
+        </div>
+        <div className="details">
+          <span>Pressure</span>
+          <span className="detail">1015hPa</span>
+        </div>
+        <div className="details">
+          <span>Humidity</span>
+          <span className="detail">64%</span>
+        </div>
+        <div className="details">
+          <span>Wind speed</span>
+          <span className="detail">35km/h</span>
+        </div>
+      </div>
+      <div className="more-details">
+        <div className="hourly">
+          <div className="hour">
+            <span className="hourly-temp">5°C</span>
+            <span className="hourly-desc">Cloudy</span>
+            <span><FaWind></FaWind> 12km/h</span>
+          </div>
+        </div>
       </div>
     </div>
   );
