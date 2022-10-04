07fecd3952bddfa149bb81c6422efccd42516ac6
downred <redonqerimi@gmail.com>
2022-10-04 14:47:35 +0200

    finish weather component

--- src/components/hourly-weather/hour.jsx
+++ src/components/hourly-weather/hour.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "../modules/hourly-weather.css";
 import { FaWind } from "react-icons/fa";
+import '../../css/weather-icons.css'
+import '../../css/weather-icons.min.css'
+
 
 const Hour = () => {
   return (
     <div className="hour">
+      <i class="wi wi-day-sunny-overcast wi-fw wi-2x w-icon"></i>
       <span className="hourly-temp">
         <span>5</span>°C
       </span>
